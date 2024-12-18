8461a51a117ff2f70e0ae92c26786d5f6e0ec097
antoine cabanes <antoine.cabanes@ynov.com>
2024-12-18 14:26:43 +0100

    ll

---
+++ CRUD/config.php
@@ -0,0 +1,19 @@
+<?php
+// config.php
+define('API_KEY', '6f5b88ad73d84dc585b132034241812');
+define('BASE_URL', 'https://api.weatherapi.com/v1/');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'weather_api');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Connexion à la base de données
+function getDatabaseConnection() {
+    try {
+        return new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    } catch (PDOException $e) {
+        die('Erreur de connexion : ' . $e->getMessage());
+    }
+}
